a4a687775f21a1a55faafa755e91be45e938b289
dany92 <dany@bc.edu>
2016-06-15 15:05:19 -0400

    collab stage

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var app = express();
+var requestLog = [];
+
+
+app.use(function(req, res, next){
+	requestLog.push(req.method+ " "+ req.url);
+	console.log(req.method+ " "+ req.url);
+	res.send('something');
+	console.log(req.method);
+	next();
+});
+
+app.get('/news', function(req, res, next){
+	res.send('This is the news!');
+});
+
+app.get('/', function(req, res, next){
+	res.send('Hello');
+});
+
+app.listen(3000, function () {
+  console.log('Server listening');
+});
